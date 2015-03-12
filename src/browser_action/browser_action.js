46b778a2d9ac4ddd590f857ff4196a965eefd167
Camille Roussel <camille@rousselfamily.com>
2015-03-12 16:26:46 -0700

    message passing stubs

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,4 +1,6 @@
 'use strict';
+var appStatus = 'Off';
+
 //Firebase.enableLogging(true);
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
   var currentTab = CryptoJS.SHA1(tabs[0].url);
@@ -17,5 +19,29 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         document.getElementById('contents').innerHTML = s.val();
       });
     });
-
 });
+
+
+var turnOn = function () {
+  chrome.extension.sendMessage({action: "On"}, function (response) {
+    console.log(response.status);
+  });
+};
+
+var turnOff = function () {
+  chrome.extension.sendMessage({action: "On"}, function (response) {
+    console.log(response.status);
+  });
+};
+
+var toggleAppStatus = function() {
+  if (appStatus === 'On') {
+    appStatus = 'Off';
+    document.querySelector('button.on-off')[0].innerHTML = 'Turn Graffiome On';
+    turnOff();
+  } else {
+    appStatus = 'On';
+    document.querySelector('button.on-off')[0].innerHTML = 'Turn Graffiome Off';
+    turnOn();
+  }
+};
