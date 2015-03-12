2ca743bb98a37649a76d2d001e336894b9b2b848
Camille Roussel <camille@rousselfamily.com>
2015-03-12 11:40:02 -0700

    clicks are registering per website.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,22 +1,21 @@
 'use strict';
 //Firebase.enableLogging(true);
-var f = new Firebase('https://dazzling-heat-2465.firebaseio.com/');
-
-f.transaction(function(curr) {
-  if (isNaN(parseFloat(curr))) {
-    return 1; // initialize to 1.
-  }
-  else {
-    return curr + 1; // increment.
-  }
-}, function() {
-    // Once the transaction has completed, update the UI (and watch for updates).
-    f.on('value', function(s) {
-      document.getElementById('contents').innerHTML = s.val();
-    });
-  });
-
-
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
+  var currentTab = CryptoJS.SHA1(tabs[0].url);
+  var f = new Firebase('https://dazzling-heat-2465.firebaseio.com/web/data/' + currentTab);
+
+  f.transaction(function(curr) {
+    if (isNaN(parseFloat(curr))) {
+      return 1; // initialize to 1.
+    }
+    else {
+      return curr + 1; // increment.
+    }
+  }, function() {
+      // Once the transaction has completed, update the UI (and watch for updates).
+      f.on('value', function(s) {
+        document.getElementById('contents').innerHTML = s.val();
+      });
+    });
+
 });
