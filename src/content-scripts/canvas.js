793eabe5dc99e2113375423aa21d8f1857a5256d
Jonathan Huang <grokut@gmail.com>
2015-03-12 12:23:39 -0700

    (feature) Full page defacement
    Canvas overlays the entire page and allows for full-page defacement.
    
    Next step is to have proper canvas resizing

--- src/content-scripts/canvas.js
+++ src/content-scripts/canvas.js
@@ -8,59 +8,68 @@ var canvas, ctx, flag = false,
 var x = "black",
     y = 2;
 
+var overlayPage = {
+  zIndex: 100,
+  position: 'absolute',
+  top: 0,
+  left: 0
+};
+
 $(function(){
   
-  $('<canvas id="can" width="1100" height="1100" style="zIndex=100;position:absolute;top:0;left:0;"></canvas>')
+  $('<canvas id="can"></canvas>')
+    .css(overlayPage)
+    .attr('width', document.body.scrollWidth) // sets to max width
+    .attr('height', document.body.scrollHeight) // sets to max height
     .on('mousemove', function(e){findxy('move', e)})
     .on('mousedown', function(e){findxy('down', e)})
     .on('mouseup', function(e){findxy('up', e)})
     .on('mouseout', function(e){ findxy('out', e)})
     .appendTo('body')
 
-    canvas = document.getElementById('can');
-    ctx = canvas.getContext("2d");
-    w = canvas.width;
-    h = canvas.height;
+  canvas = document.getElementById('can');
+  ctx = canvas.getContext("2d");
+  w = canvas.width;
+  h = canvas.height;
 })
 
 function draw() {
-    ctx.beginPath();
-    ctx.moveTo(prevX+pageXOffset, prevY+pageYOffset);
-    ctx.lineTo(currX+pageXOffset, currY+pageYOffset);
-    ctx.strokeStyle = x;
-    ctx.lineWidth = y;
-    ctx.stroke();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.moveTo(prevX+pageXOffset, prevY+pageYOffset);
+  ctx.lineTo(currX+pageXOffset, currY+pageYOffset);
+  ctx.strokeStyle = x;
+  ctx.lineWidth = y;
+  ctx.stroke();
+  ctx.closePath();
 }
 
-function findxy(res, e) {
-    console.log(e.clientX, e.clientY)
-    if (res == 'down') {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
+function findxy(res, e) { 
+  if (res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
 
-        flag = true;
-        dot_flag = true;
-        if (dot_flag) {
-            ctx.beginPath();
-            ctx.fillStyle = x;
-            ctx.fillRect(currX, currY, 2, 2);
-            ctx.closePath();
-            dot_flag = false;
-        }
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+      ctx.beginPath();
+      ctx.fillStyle = x;
+      ctx.fillRect(currX, currY, 2, 2);
+      ctx.closePath();
+      dot_flag = false;
     }
-    if (res == 'up' || res == "out") {
-        flag = false;
+  }
+  if (res == 'up' || res == "out") {
+    flag = false;
+  }
+  if (res == 'move') {
+    if (flag) {
+      prevX = currX;
+      prevY = currY;
+      currX = e.clientX - canvas.offsetLeft;
+      currY = e.clientY - canvas.offsetTop;
+      draw();
     }
-    if (res == 'move') {
-        if (flag) {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-            draw();
-        }
-    }
+  }
 }
