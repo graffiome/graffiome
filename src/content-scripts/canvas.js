4f68ebde7509a118e451cedfe2e83027abbb69a3
Jonathan Huang <grokut@gmail.com>
2015-03-11 20:42:55 -0700

    Canvas HTML element is injected to current tab's DOM.
    The canvas element has these properties:
      - automatic resizing
      - fits to window size
      - drawing capability
    
    The canvas element does not fill the whole page if page requires scrolling.

--- src/content-scripts/canvas.js
+++ src/content-scripts/canvas.js
@@ -1,20 +1,68 @@
-
-// On document ready, inject canvas onto page
+// On document ready, inject canvas onto page with drawing capabilities
 $(function() {
 
-  $('body').prepend('<canvas id="canvas"></canvas>')
+  var canvas = FULL_PAGE_CANVAS.mount()
+      canvas.setAttribute('id', 'canvas');
+      canvas.style.position = 'absolute';
+      canvas.style.left="0px";
+      canvas.style.top="0px";
 
-  canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
 
-  canvas.style.position = 'absolute';
-  canvas.style.left="0px";
-  canvas.style.top="0px";
-  canvas.style.zIndex="100";
-  canvas.style.width="100%";
-  canvas.style.height="100%";
-  canvas.width=document.body.clientWidth;
-  canvas.height=document.body.clientHeight;
+  $('#canvas').mousedown(function(e){
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+      
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+  });
 
-  context = canvas.getContext("2d");
+  $('#canvas').mousemove(function(e){
+    if(paint){
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+      redraw();
+    }
+  });
+
+  $('#canvas').mouseup(function(e){
+    paint = false;
+  });
+
+  $('#canvas').mouseleave(function(e){
+    paint = false;
+  });
+
+  var clickX = new Array();
+  var clickY = new Array();
+  var clickDrag = new Array();
+  var paint;
+
+  function addClick(x, y, dragging)
+  {
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+  }
+
+  function redraw(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+        
+    for(var i=0; i < clickX.length; i++) {    
+      context.beginPath();
+      if(clickDrag[i] && i){
+        context.moveTo(clickX[i-1], clickY[i-1]);
+       }else{
+         context.moveTo(clickX[i]-1, clickY[i]);
+       }
+       context.lineTo(clickX[i], clickY[i]);
+       context.closePath();
+       context.stroke();
+    }
+  }
 
 });
