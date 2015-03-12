4d7590e3475b881b86b89fd3dd5091bcb923adcb
Jonathan Huang <grokut@gmail.com>
2015-03-12 11:21:40 -0700

    Canvas spans full-page but drawing only occurs on the top half.

--- src/content-scripts/canvas.js
+++ src/content-scripts/canvas.js
@@ -1,68 +1,68 @@
-// On document ready, inject canvas onto page with drawing capabilities
-$(function() {
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
 
-  var canvas = FULL_PAGE_CANVAS.mount()
-      canvas.setAttribute('id', 'canvas');
-      canvas.style.position = 'absolute';
-      canvas.style.left="0px";
-      canvas.style.top="0px";
+var x = "black",
+    y = 2;
 
-  var context = canvas.getContext("2d");
+$(function(){
+  
+ 
 
-  $('#canvas').mousedown(function(e){
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
-      
-    paint = true;
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    redraw();
-  });
+  $('<canvas id="can" width="1100" height="1100" style="zIndex=100;position:absolute;top:0;left:0;"></canvas>')
+    .appendTo('body')
+    .on('mousemove', function(e){findxy('move', e)})
+    .on('mousedown', function(e){findxy('down', e)})
+    .on('mouseup', function(e){findxy('up', e)})
+    .on('mouseout', function(e){ findxy('out', e)})
 
-  $('#canvas').mousemove(function(e){
-    if(paint){
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-      redraw();
+    canvas = document.getElementById('can');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+})
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function findxy(res, e) {
+    console.log(e.clientX, e.clientY)
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
     }
-  });
-
-  $('#canvas').mouseup(function(e){
-    paint = false;
-  });
-
-  $('#canvas').mouseleave(function(e){
-    paint = false;
-  });
-
-  var clickX = new Array();
-  var clickY = new Array();
-  var clickDrag = new Array();
-  var paint;
-
-  function addClick(x, y, dragging)
-  {
-    clickX.push(x);
-    clickY.push(y);
-    clickDrag.push(dragging);
-  }
-
-  function redraw(){
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-    
-    context.strokeStyle = "#df4b26";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-        
-    for(var i=0; i < clickX.length; i++) {    
-      context.beginPath();
-      if(clickDrag[i] && i){
-        context.moveTo(clickX[i-1], clickY[i-1]);
-       }else{
-         context.moveTo(clickX[i]-1, clickY[i]);
-       }
-       context.lineTo(clickX[i], clickY[i]);
-       context.closePath();
-       context.stroke();
+    if (res == 'up' || res == "out") {
+        flag = false;
     }
-  }
-
-});
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
+}
