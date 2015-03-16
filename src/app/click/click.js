0950d5dd0dfd5c82d48ac38e2b56ae3597e18aeb
Rich Magee <rmagee88@gmail.com>
2015-03-16 11:38:06 -0700

    Fixed button for changing/showing app state, shows correclty based on status and updates correctly to turn app on and off

--- src/app/click/click.js
+++ src/app/click/click.js
@@ -11,7 +11,10 @@ angular.module('graffio.clickController', [])
     $state.go('login');
   };
 }).controller('onOffController', function($scope){ 
-//TODO: get state from background, set state, message content script
+  // initialize text before we can query the current tab
+  $scope.onOffButtonTxt = 'loading...';
+
+  // helper function to determine what the current tab is and perform a callback on that tabID value
   var getCurrentTabID = function(callback) {
     chrome.tabs.query( {currentWindow: true, active: true}, function(tabs){
       var currentTabId = tabs[0].id;
@@ -19,6 +22,8 @@ angular.module('graffio.clickController', [])
     });
   };
 
+  // getStatus takes a callback and applies it to the status of the current tab
+  // it queries the current tab for the status of the app on that tab
   var getStatus = function(callback) {
     getCurrentTabID(function(tabID) {
       chrome.tabs.sendMessage(tabID, {getStatus: true}, function(res) {
@@ -26,25 +31,58 @@ angular.module('graffio.clickController', [])
       });
     });
   };
-  
-  $scope.toggleStatus = function() {
-    getStatus(function(status, tabID) {
-      var msg;
+
+  // generic UI update function for the status of the app
+  // needs to use $scope.$apply since these callback functions otherwise wouldn't trigger a $digest event
+  // even though they would update the $scope variable values...
+  // $scope.$apply triggers the $digest, which in turn is what causes a UI update
+  var setStatusUi = function(status) {
+    console.log('setStatusUI called...');
+    console.log('setStatusUI status: ', status);
+    $scope.$apply(function() {
       if (status === 'off') {
-        msg = 'on';
-        $scope.onOffButtonTxt = 'Off';
-      } else {
-        msg = 'off';
         $scope.onOffButtonTxt = 'On';
+      } else {
+        $scope.onOffButtonTxt = 'Off';
       }
-      chrome.tabs.sendMessage(tabID, {toggle: msg}, function(res){
-        console.log('toggleStatus:', res);
-      });
-    });
+    })
   };
 
-  getStatus(function(res) {
-    $scope.onOffButtonTxt = res.status;
+  // generic send tab message function, telling the content script to change from
+  // the current status to the opposite
+  var sendTabMessage = function(status, tabID) {
+    var msg;
+    if (status === 'off') {
+      msg = 'on';
+    } else {
+      msg = 'off';
+    }
+    chrome.tabs.sendMessage(tabID, {toggle: msg}, function(res){
+      console.log('toggleStatus:', res);
+    });
+  };
+    
+  // function called when button is pressed by user wishing to toggle the current state
+  $scope.toggleStatus = function() {
+    // figure out what existing state is from the content script
+    getStatus(function(status, tabID) {
+      // send a message to the tab and also set the current button value to be the opposite
+      // ie. if a user clicks 'On' it should send a message telling the app to start drawing
+      // and also change the UI here to indicate that the next click will turn the app off
+      sendTabMessage(status, tabID);
+      if (status === 'off') {
+        setStatusUi('on');  
+      } else {
+        setStatusUi('off');
+      }
+      
+    });
+  };
+ 
+  console.log('initial get status called...');
+  // Initiall call to getStatus to figure out what status the page was in last.
+  getStatus(function(status, tabID) {
+    setStatusUi(status);
     console.log('status set');
   });
 });
