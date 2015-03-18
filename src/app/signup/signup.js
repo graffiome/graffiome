41aab49e22232aab0d0f36f8850ce925be6b7b75
Camille Roussel <camille@rousselfamily.com>
2015-03-18 11:43:02 -0400

    Waiting on mrBlueBlue PR merge before tackling canvas.

--- src/app/signup/signup.js
+++ src/app/signup/signup.js
@@ -1,8 +1,7 @@
+'use strict';
 angular.module('graffio.signupController', [])
 .controller('signupController', function($scope, $state) {
-  var ref = new Firebase("https://dazzling-heat-2465.firebaseio.com");
-  $scope.email;
-  $scope.password;
+  var ref = new Firebase('https://dazzling-heat-2465.firebaseio.com');
 
   $scope.signUp = function() {
     // firebase creation of new user
@@ -11,9 +10,9 @@ angular.module('graffio.signupController', [])
       password : $scope.password
     }, function(error, authData) {
       if (error) {
-        console.log("Error creating user:", error);
+        console.log('Error creating user:', error);
       } else {
-        console.log("Successfully created user account with uid:", authData.uid);
+        console.log('Successfully created user account with uid:', authData.uid);
 
         // now we need to log that user in to get their access token
         ref.authWithPassword({
@@ -21,19 +20,15 @@ angular.module('graffio.signupController', [])
           password : $scope.password
         }, function(error, authData) {
           if (error) {
-            console.log("Login Failed!", error);
+            console.log('Login Failed!', error);
           } else {
-            console.log("Authenticated successfully with payload:", authData);
+            console.log('Authenticated successfully with payload:', authData);
             // send the token to the background script so it can be accessed by each tab
             chrome.runtime.sendMessage({auth: authData, action: 'setToken'});
             $state.go('main');
           }
         });
-
       }
     });
   };
-
 });
-
-
