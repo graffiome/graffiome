3d6ab8a1a416d984ed2f157427bae6cc2817b5aa
Jonathan Huang <grokut@gmail.com>
2015-03-23 14:21:00 -0700

    working phantom test

--- spec/run.js
+++ spec/run.js
@@ -1,10 +1,26 @@
 var node_modules = '../node_modules/';
+var src = '../src/content-scripts/';
+var lib = '../bower_components/';
+
 phantom.injectJs(node_modules + 'mocha/mocha.js');
 phantom.injectJs(node_modules + 'chai/chai.js');
 phantom.injectJs(node_modules + 'sinon-chrome/phantom-tweaks.js');
+
+phantom.injectJs(lib + 'jquery/dist/jquery.min.js');
+
+phantom.injectJs(lib + 'firebase/firebase.js');
+phantom.injectJs(lib + 'mockfirebase/browser/mockfirebase.js');
+phantom.injectJs(lib + 'CryptoJS/rollups/sha1.js');
+// phantom.injectJs(lib + 'CryptoJS/rollups/sha1.js');
+
+phantom.injectJs(src + 'canvas.js');
+
 mocha.setup({ui: 'bdd', reporter: 'spec'});
+
+
 phantom.injectJs('beforeeach.js');
 phantom.injectJs('canvas.test.js');
+
 mocha.run(function(failures) {
   // setTimeout is needed to supress "Unsafe JavaScript attempt to access..."
   // see https://github.com/ariya/phantomjs/issues/12697
