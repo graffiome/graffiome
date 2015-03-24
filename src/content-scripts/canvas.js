b3463149221317adde3c10add762aafbd9411140
Camille Roussel <camille@rousselfamily.com>
2015-03-23 17:19:56 -0700

    moved firebase references to background to reduce concurrent users

--- src/content-scripts/canvas.js
+++ src/content-scripts/canvas.js
@@ -13,21 +13,23 @@ var toggle = 'off';
 
 var tabUrl = CryptoJS.SHA1(document.URL).toString();
 
-var ref = new Firebase('https://dazzling-heat-2465.firebaseio.com/web/data/sites/' + tabUrl);
-var onValueChange;
-
-var getCurrentUser = function(){
-  return ref.getAuth() ? ref.getAuth().uid.replace(':','') : null;
+var getCurrentUser = function(callback){
+  chrome.runtime.sendMessage({action: 'getUser'}, function(response) {
+    callback(response.user);
+  });
 };
 
 var saveUserCanvas = function(){
   var data = canvas.toDataURL();
-  if (getCurrentUser()){
-    ref.child(getCurrentUser()).set(data);
-    console.log('saving user canvas');
-  } else {
-    console.log('failed to save canvas');
-  }
+  chrome.runtime.sendMessage(
+    {action: 'saveCanvas', site: tabUrl, data: data}, 
+    function(response) {
+      if (response.saveStatus) {
+        console.log('saving user canvas');
+      } else {
+        console.log('failed to save canvas');
+    }
+  });
 };
 
 var drawLine = function(){
@@ -76,8 +78,10 @@ var turnEditOn = function($canvas){
     })
     .on('mouseout', function(e){ findxy('out', e);})
 
-  canvas = document.getElementsByClassName(getCurrentUser())[0];
-  ctx = canvas.getContext('2d');
+  getCurrentUser(function(user){
+    canvas = document.getElementsByClassName(user)[0];
+    ctx = canvas.getContext('2d');
+  }); 
 };
 
 var turnEditOff = function($canvas){
@@ -104,22 +108,26 @@ var drawCanvasElement = function(context, data){
 
 var toggleUserCanvasOn = function(){
   if ( toggle === 'off' ) {
-    var userCanvas = $('.'+ getCurrentUser());
-    if (userCanvas.length === 0){
-      appendCanvasElement(getCurrentUser());
-      userCanvas = $('.'+getCurrentUser());
-      turnEditOn(userCanvas);
-    } else {
-      turnEditOn(userCanvas);
-    }  
-    toggle = 'on';
+    getCurrentUser(function(user){
+      var userCanvas = $('.' + user);
+      if (userCanvas.length === 0){
+        appendCanvasElement(user);
+        userCanvas = $('.' + user);
+        turnEditOn(userCanvas);
+      } else {
+        turnEditOn(userCanvas);
+      }  
+      toggle = 'on';
+    });
   }
 };
 
 var toggleUserCanvasOff = function(){
-  var userCanvas = $('.'+getCurrentUser());
-  turnEditOff(userCanvas);
-  toggle = 'off';
+  getCurrentUser(function(user){
+    var userCanvas = $('.' + user);
+    turnEditOff(userCanvas);
+    toggle = 'off';
+  });
 };
 
 var removeGraffeoCanvasAll = function(){
@@ -132,35 +140,15 @@ var clearUserCanvas = function(){
   saveUserCanvas();
 };
 
-var userLogout = function() {
-  ref.unauth();
-  ref.off('value', onValueChange);
-  removeGraffeoCanvasAll();
-};
-
-var userLogin = function(token) {
-  ref.authWithCustomToken(token, function(error) {
-    if (error) { 
-      console.log('Login Failed!', error); 
-    } else {
-      onValueChange = ref.on('value', function(snapshot){
-        var allCanvases = snapshot.val();
-        if (getCurrentUser() && allCanvases) {
-          for (var user in allCanvases){
-            var data = allCanvases[user];
-            var context;
-            if (document.getElementsByClassName(user)[0]) {
-              context = document.getElementsByClassName(user.replace(':',''))[0].getContext('2d');  
-            } else {
-              appendCanvasElement(user.replace(':',''));
-              context = document.getElementsByClassName(user.replace(':',''))[0].getContext('2d');
-            }
-            drawCanvasElement(context, data);
-          }
-        }
-      });
+var onCanvasData = function(site, user, data) {
+  // if the user does not already have a canvas
+  if (tabUrl === site) {
+    if (!document.getElementsByClassName(user)[0]) {
+      appendCanvasElement(user);
     }
-  });
+    var context = document.getElementsByClassName(user)[0].getContext('2d');  
+    drawCanvasElement(context, data);
+  }
 };
 
 var eraseUserCanvas = function(){
@@ -173,14 +161,14 @@ var addImage = function(src, event){
   img.src = chrome.extension.getURL(src);
   img.onload = function(){
     ctx.drawImage(img, event.clientX - 25, event.clientY - 25);
-  }
-}
+  };
+};
 
 var addOneTimeClickEvent = function(element, callback, src){
   element.on('click', function(event){
     callback(src, event);
     element.off('click');
-  })
+  });
 };
 
 // Message Handler
@@ -197,30 +185,25 @@ chrome.runtime.onMessage.addListener(
     // Initialize toggle status for popup button
     } else if ( request.getStatus === true ){
       sendResponse({status:toggle});
-    } else if (request.updateToken) { // change in user Authentication status.
-      if (request.token === null) {
-        userLogout();
-      } else {
-        userLogin(request.token);
-      }
-
+    } else if (request.canvasData) { // new Canvas data
+      onCanvasData(request.site, request.user, request.data);
     } else if (request.erase){
       eraseUserCanvas();
     } else if (request.changeColor){
       lineColor = request.changeColor;
-
     } else if (request.image){
-      var userCanvas = $('.'+ getCurrentUser());
-      addOneTimeClickEvent(userCanvas, addImage, request.image);
+      getCurrentUser(function(user){
+        var userCanvas = $('.'+ user);
+        addOneTimeClickEvent(userCanvas, addImage, request.image);
+      });
     }
   }
 );
 
-// Check to see if the user is already logged in when the page loads.
-chrome.runtime.sendMessage({action: 'getToken'}, function(response) {
-  if (response.token === null) {
-    userLogout();
-  } else {
-    userLogin(response.token);
-  }
+// Register for data for the site
+chrome.runtime.sendMessage({action: 'startSiteData', site: tabUrl});
+
+// Unregister for data for the site
+$(window).unload(function (){
+  chrome.runtime.sendMessage({action: 'stopSiteData', site: tabUrl});
 });
